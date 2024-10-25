7be91ccdc641d6cdcc8b3cabeb42c6f121fed847
jitcoder <israfil334@gmail.com>
2024-10-25 20:32:41 +0500

    initial commit

---
+++ statistics/go.mod
@@ -0,0 +1,3 @@
+module github.com/proxetas/libnethawk/statistics
+
+go 1.23.2
---
+++ statistics/statistics.go
@@ -0,0 +1,37 @@
+package statistics
+
+import "time"
+
+type Stat struct {
+	SYN                uint32 `json:"syn"`
+	ACK                uint32 `json:"ack"`
+	FIN                uint32 `json:"fin"`
+	RST                uint32 `json:"rst"`
+	Packets            uint32 `json:"packets"`
+	PacketsOutbound    uint32 `json:"packetsOutbound"`
+	Fragmented         uint32 `json:"fragmented"`
+	UdpPackets         uint32 `json:"udpPackets"`
+	UdpPacketsOutbound uint32 `json:"udpPacketsOutbound"`
+}
+
+type RateStat struct {
+	Stat
+	Snapshot     Stat
+	SnapshotTime time.Time
+}
+
+func SubtractStats(a *Stat, b *Stat) *Stat {
+	result := &Stat{
+		SYN:                a.SYN - b.SYN,
+		ACK:                a.ACK - b.ACK,
+		FIN:                a.FIN - b.FIN,
+		RST:                a.RST - b.RST,
+		Packets:            a.Packets - b.Packets,
+		PacketsOutbound:    a.PacketsOutbound - b.PacketsOutbound,
+		UdpPackets:         a.UdpPackets - b.UdpPackets,
+		UdpPacketsOutbound: a.UdpPacketsOutbound - b.UdpPacketsOutbound,
+	}
+
+	return result
+}
+
